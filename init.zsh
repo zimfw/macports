ee4348ca29806901504d7842841a08099747d48d
Eric Nielsen <4120606+ericbn@users.noreply.github.com>
2025-01-08 19:38:23 -0500

    Simplify code

--- init.zsh
+++ init.zsh
@@ -1,22 +1,22 @@
 (( ${+commands[port]} )) || return 1
 
-local popts=
+local sudoport='sudo port'
 if [[ -e /usr/local/Cellar ]]; then
   # Enable trace mode if Homebrew is installed in /usr/local
-  popts=' -t'
+  sudoport+=' -t'
 fi
-alias portc="sudo port${popts} clean --all installed"
-alias portC="sudo port${popts} reclaim"
+alias portc=${sudoport}' clean --all installed'
+alias portC=${sudoport}' reclaim'
 alias portd='port diagnose'
 alias porti='port info'
-alias portI="sudo port${popts} -c install"
+alias portI=${sudoport}' install'
 alias portl='port installed'
 alias portL='port installed requested'
 alias portn='port notes'
 alias porto='port outdated'
 alias ports='port search'
-alias portu="sudo port${popts} selfupdate"
-alias portU="sudo port${popts} -u upgrade outdated"
+alias portu=${sudoport}' selfupdate'
+alias portU=${sudoport}' -u upgrade outdated'
 alias portv='port variants'
-alias portx="sudo port${popts} --follow-dependencies uninstall"
-unset popts
+alias portx=${sudoport}' --follow-dependencies uninstall'
+unset sudoport
